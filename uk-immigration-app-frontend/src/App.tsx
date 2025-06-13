85b3813ff89e822ebb6bfc68a80ba69d221cc921
Lotanna <tannaloamobi@gmail.com>
2025-06-13 05:59:00 +0100

    added sample background image to page background

--- uk-immigration-app-frontend/src/App.tsx
+++ uk-immigration-app-frontend/src/App.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Box from "@mui/material/Box";
+import backgroundImage from './UK-immigration-background.jpg'
+
 
 const App = () => {
   return (
     <div>
-          <Header />
-          <Footer />
+      <Header />
+      <Box
+        sx={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "contain",
+          height: "100vh",
+          width: "100vw",
+        }}
+      />
+      <Footer />
     </div>
   );
 }
