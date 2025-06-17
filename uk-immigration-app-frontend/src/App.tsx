d8f213fadb64d431618c472e8c0f44b1942f3870
Lotanna <tannaloamobi@gmail.com>
2025-06-17 20:10:40 +0100

    created infohub, home and absenceCounter routes

--- uk-immigration-app-frontend/src/App.tsx
+++ uk-immigration-app-frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import InfoHub from "./pages/InfoHub";
+import AbsenceCounter from "./pages/AbsenceCounter";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/infoHub" element={<InfoHub />} />
+        <Route path="/absenceCounter" element={<AbsenceCounter />} />
       </Routes>
     </BrowserRouter>
   );
--- uk-immigration-app-frontend/src/pages/Home.tsx
+++ uk-immigration-app-frontend/src/pages/Home.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import "./App.css";
 import Header from '../components/Header';
 import Footer from "../components/Footer";
 import Box from "@mui/material/Box";
-import backgroundImage from "./TravelPhoto.jpg";
+import backgroundImage from "../TravelPhoto.jpg";
 
 const Home = () => {
   return (
