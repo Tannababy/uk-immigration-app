f7c20bcea2778768b1ca913a8d04c6aeb7c9be71
Lotanna <tannaloamobi@gmail.com>
2025-11-15 01:38:01 +0000

    deleted setters in holiday model

--- uk-immigration-app-backend/src/main/java/com/ukimigrationcalc/uk_immigration_calc_backend/model/Holiday.java
+++ uk-immigration-app-backend/src/main/java/com/ukimigrationcalc/uk_immigration_calc_backend/model/Holiday.java
@@ -14,15 +14,9 @@ public class Holiday {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getDate() {
         return date;
     }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
 }
